e575d13c2566fbc9c55d694af0d1c2d4654b5737
JC <marybecker053@gmail.com>
2024-05-12 12:57:34 -0500

    buttons done

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,30 @@
-const Buttons = ({ filterByType, setSat, displaySats }) => {
+import React from "react";
+
+const Buttons = ({ filterByType, setSat, orbitTypes }) => {
+  const handleAllSatellites = () => {
+    setSat(satData);
+  };
+
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      {orbitTypes.map((type, index) => (
+        <button onClick={() => filterByType(type)} key={index}>
+          {type} Orbit
+        </button>
+      ))}
+      <button onClick={() => setSat(satData)}>All Orbits</button>
     </div>
   );
 };
 
 export default Buttons;
+
+
+// {orbitTypes.map((type, index) => {
+//   return (
+//     <button onClick={() => filterByType(sat)} key={index}>
+//       {type} Orbit
+//     </button>
+//   );
+// })}
+//code continues
